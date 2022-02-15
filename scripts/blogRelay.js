e763c1a2f19cb6cee6537e6f3cf2d58ca316ae67
mehm8128 <mehm8128@gmail.com>
2022-02-15 12:01:43 +0900

    test

--- scripts/blogRelay.js
+++ scripts/blogRelay.js
@@ -1,25 +1,24 @@
 const fetch = require('node-fetch')
 const CronJob = require('cron').CronJob
 
-const above = {
-  '2/15': 'mehm8128',
-  '2/16': 'mehm',
-}
-const below = {
-  '2/15': 'mehm8128',
-  '2/16': 'mehm',
-}
-
-const channelID = 'e93ef204-1e25-4456-90bf-9214a2e6684e'
-//ブログリレーID'22edf673-352f-4f18-88a1-201e681bc483'
-
 module.exports = (robot) => {
+  const above = {
+    '2/15': 'mehm8128',
+    '2/16': 'mehm',
+  }
+  const below = {
+    '2/15': 'mehm8128',
+    '2/16': 'mehm',
+  }
+  const channelID = 'e93ef204-1e25-4456-90bf-9214a2e6684e'
+  //ブログリレーID'22edf673-352f-4f18-88a1-201e681bc483'
   const today = new Date()
   const todayMonth = today.getMonth() + 1
   const todayDate = today.getDate()
   const todayString = `${todayMonth}/${todayDate}`
   const tommorowString = `${todayMonth}/${todayDate + 1}`
-  const cron = new CronJob('51 11 * * *', () => {
+  //const cron = new CronJob('51 11 * * *', () => {
+  robot.respond(/remind/, (res) => {
     if (todayString in below && !(tommorowString in below)) {
       robot.send(
         { channelID: channelID },
@@ -33,7 +32,7 @@ module.exports = (robot) => {
     } else if (todayString in below && tommorowString in below) {
       robot.send(
         { channelID: channelID },
-        `@${above[todayString]} ,@${above[todayString]} 今日です\n@${above[tommorowString]} ,@${above[todayString]} 明日です`
+        `@${above[todayString]} ,@${above[todayString]} 今日です\n@${above[tommorowString]} ,@${above[tommorowString]} 明日です`
       )
     } else if (!(todayString in below) && !(tommorowString in below)) {
       robot.send(
