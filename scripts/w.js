07efa6ab2c0083bd176c6bc3609940f64195dd6e
mehm8128 <mehm8128@gmail.com>
2022-08-11 22:09:48 +0900

    add month feature & env variable

--- scripts/w.js
+++ scripts/w.js
@@ -27,6 +27,11 @@ const oneDaySecond = 1000 * 60 * 60 * 24
 module.exports = (robot) => {
   robot.respond(/草.*/i, async (response) => {
     const userId = response.message.message.plainText.split(' ')[2]
+    const monthArg = Number(response.message.message.plainText.split(' ')[3])
+    if (isNaN(monthArg) && monthArg !== '') {
+      response.send('月の指定が不正です')
+      return
+    }
     let userUuid = ''
     const url = `https://q.trap.jp/api/v3/users?include-suspended=false&name=${userId}`
     flag = false
@@ -56,11 +61,13 @@ module.exports = (robot) => {
     for (let i = -1; i < 27; i++) {
       const day = new Date(today.getTime() - i * oneDaySecond)
       const year = day.getFullYear()
-      const month = (day.getMonth() + 1).toString().padStart(2, '0')
+      const month = (day.getMonth() + 1 - monthArg).toString().padStart(2, '0')
       const date = day.getDate().toString().padStart(2, '0')
       const prevDay = new Date(today.getTime() - (i + 1) * oneDaySecond)
       const prevYear = prevDay.getFullYear()
-      const prevMonth = (prevDay.getMonth() + 1).toString().padStart(2, '0')
+      const prevMonth = (prevDay.getMonth() + 1 - monthArg)
+        .toString()
+        .padStart(2, '0')
       const prevDate = prevDay.getDate().toString().padStart(2, '0')
       const url = `https://q.trap.jp/api/v3/messages?word=&after=${prevYear}-${prevMonth}-${prevDate}T00%3A00%3A00.000Z&before=${year}-${month}-${date}T00%3A00%3A00.000Z&from=${userUuid}&limit=1&offset=0&sort=createdAt`
       await fetch(url, {
