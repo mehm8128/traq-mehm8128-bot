27f3d4fc03378983c40667ce66dae8bd6e782991
mehm8128 <mehm8128@gmail.com>
2022-11-24 10:38:15 +0900

    start advent calendar

--- scripts/blogRelay.js
+++ scripts/blogRelay.js
@@ -1,11 +1,43 @@
 const CronJob = require('cron').CronJob
 
 module.exports = (robot) => {
-  const list = {}
-  const channelID = ''
+  const list = {
+    '11/25': '@mehm8128',
+    '11/26': '@Uzaki',
+    '11/27': '@oribe',
+    '11/28': '',
+    '11/29': '@Ras',
+    '11/30': '@helgev',
+    '12/01': '@Hosoi',
+    '12/02': '',
+    '12/03': '',
+    '12/04': '@pikachu',
+    '12/05': '',
+    '12/06': '@s9',
+    '12/07': '@inutamago_dogegg',
+    '12/08': '@Uzaki @inutamago_dogegg',
+    '12/09': '@Hmcmch',
+    '12/10': '@YHz_ikiri',
+    '12/11': '@quesera @noc7t',
+    '12/12': '@Hinaruhi',
+    '12/13': '@d_etteiu8383',
+    '12/14': '@Takeno_hito',
+    '12/15': '@mera',
+    '12/16': '@nosaerc',
+    '12/17': '@H1rono_K',
+    '12/18': '@hijiki51',
+    '12/19': '@toshi00',
+    '12/20': '@dan_dan',
+    '12/21': '@mehm8128',
+    '12/22': '@toruthi',
+    '12/23': '@Uzaki',
+    '12/24': '@ikura-hamu',
+    '12/25': '',
+  }
+  const channelID = '706f39fe-83f6-4f66-873d-4a8153e441a3'
 
   const cron = new CronJob(
-    '00 10 * * *',
+    '40 10 * * *',
     () => {
       const today = new Date()
       const tomorrow = new Date(today.getTime() + 1000 * 60 * 60 * 24)
