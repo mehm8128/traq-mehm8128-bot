03b517486d05a0a9c2e4e8895fb92b1a5931d1e4
mehm8128 <mehm8128@gmail.com>
2022-12-01 14:55:39 +0900

    add member

--- scripts/blogRelay.js
+++ scripts/blogRelay.js
@@ -28,7 +28,7 @@ module.exports = (robot) => {
     '12/18': '@hijiki51',
     '12/19': '@toshi00',
     '12/20': '@dan_dan',
-    '12/21': '@mehm8128',
+    '12/21': '@mehm8128 @kanibaku8',
     '12/22': '@toruthi @irori',
     '12/23': '@Uzaki',
     '12/24': '@ikura-hamu',
